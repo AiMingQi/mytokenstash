06f33c78dc3fbe07e08700b9e94cc1683eed5c11
AiMingQi <cafevalentine@gmail.com>
2022-01-22 13:00:40 -0700

    privacy policy update

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -7,23 +7,28 @@
         cols="12"
         sm="6"
       >
+      <v-card dark>
+      <v-card-title>
         <h2 class="headline font-weight-bold mb-6">
           Privacy Policy:
         </h2>
+      </v-card-title>
 
-        <v-row class="text-left">
-        <p>MyTokenStash is deployed using IPFS and therefore the creators of MyTokenStash have no record or knowledge of how users use the app. Users are fully responsible for the results of using the app.</p>
-        <p>MyTokenStash is a completely decentralized dApp. Therefore it DOES NOT use cookies, trackers, tracked users, stored users, centralized databases, google analytics, any analytics, any record of page usage or any means of knowing who, what, when, where or why anyone uses the Javascript or Typescript libraries on their own device to interact with a decentralized blockchain ledger technology. </p>
+      <v-card-text>
+        <p>MyTokenStash.com protects your privacy by design. You are in complete control of every moment that shares your data publicly (Only when you submit a valid transaction which changes the blockchain state).</p>
+        <p>MyTokenStash is deployed using IPFS and by design the creators of MyTokenStash have no record or knowledge of who, what, when, where or why users use the app. Users are fully responsible for the results of using the app.</p>
+        <p>MyTokenStash is a completely decentralized dApp. Therefore it DOES NOT use cookies, trackers, tracked users, stored users, centralized databases, google analytics, any analytics, any record of page usage or any means of knowing who, what, when, where or why anyone uses the Javascript or Typescript libraries on their own device to interact with decentralized public blockchain ledger technologies to make read and write activities on those public ledgers. </p>
         <p>When a user connects their wallet or inputs a public key into the application, all processing logic happens locally on the user's device.  Only in the event that a user initiates a signed transaction utilizing JSON RPC calls to publicly available RPC channels is there a possibility that some form of universally recognized state change can occur. </p>
         <p>MyTokenStash is a tool for interacting with public blockchains, and security is determined by the blockchain technology itself.  This site can form Typescript and Javascript transaction requests, utilizing public RPC Channels and is transparent about all aspects of transaction construction, and all code is visible at https://github.com/aimingqi/mytokenstash  </p>
-        <p>Due to the fact that all code is public and reproducable it is extremely likely that many different actors will reproduct the MyTokenStash.com website and may attempt nepharious actions.  Users are wise to always remain diligent and wary of anything suspicious. </p>
+        <p>Due to the fact that all code is public and reproducable it is extremely likely that many different actors will reproduct the MyTokenStash.com website and may attempt nepharious actions.  Users are wise to always remain diligent and wary of anything suspicious and do their own code audits and domain checks to ensure they are fully knowledgable about their actions. </p>
         <p>MyTokenStash is not responsible for, and will not be able to assist users in any way with retriving funds, creating refunds, arbitrating disbutes, turning back transactions, solving any problems other than are strictly related to the functionality of MyTokenStash itself.  </p>
-        <p>All issues can be submitted to https://github/aimingqi/mytokenstash as proper Pull Requests and may or not be merged. </p>
+        <p>All issues can be submitted to <a href="https://github.com/aimingqi/mytokenstash" target="_blank">https://github/aimingqi/mytokenstash</a> as pull requests. </p>
         <p> </p>
         <p> </p>
         <p> </p>
-        </v-row>
+      </v-card-text>
         
+        </v-card>
       </v-col>
       <v-col cols="12" sm="6">
         <v-card class="ma-3">
@@ -31,7 +36,7 @@
             Important Resources:
           </v-card-title>
           <v-card-text>
-            <h3><a href="https://github.com/aimingqi/mytokenstash" target="_blank"><v-icon x-large>mdi-github</v-icon></a> All code is found at our public github repo.</h3>
+            <h3><a class="text--red" href="https://github.com/aimingqi/mytokenstash" target="_blank"><v-icon x-large>mdi-github</v-icon></a> All code is found at our public github repo.</h3>
             
           </v-card-text>
         </v-card>
@@ -40,3 +45,7 @@
   </v-container>
   </div>
 </template>
+
+<style>
+a {color: #c00000 !important;}
+</style>
