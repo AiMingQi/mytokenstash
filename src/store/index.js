24be04022542c884ea74790cde2a2ec48d5ad5cd
AiMingQi <cafevalentine@gmail.com>
2022-01-28 12:37:57 -0700

    store simplifcatoin

--- src/store/index.js
+++ src/store/index.js
@@ -12,22 +12,6 @@ export default new Vuex.Store({
       'testnet',
       'mainnet-beta',
     ],
-    initiatorNFT: {
-      name: 'Initiator',
-      mint: 'Please Choose Initiator NFT',
-      image: 'https://gateway.pinata.cloud/ipfs/QmW5JjSHfXvrdHxAYoeKK3hCvaGSYWBDJ2Pz5XJPovfUtc',
-      description: 'The Person Initiating the Match',
-      age: 'unknown'
-    },
-    challengerNFT: {
-      name: 'Challenger',
-      mint: 'Please Elect Challenger',
-      image: 'https://gateway.pinata.cloud/ipfs/QmW5JjSHfXvrdHxAYoeKK3hCvaGSYWBDJ2Pz5XJPovfUtc',
-      description: 'The Challenger to the Match',
-      age: 'unknown'
-    },
-    initiatorNFTaddress: 'Please Choose Initiator NFT',
-    challengerNFTaddress: 'Please Elect Challenger'
   },
   mutations: {
     updateNetwork (state, network) {
